8d5df98ffaad71e94abbb7312e5128a3130e6e8d
ChuguevRoman <5tiger5xman@gmail.com>
2023-02-14 20:30:02 +0200

    Created location_screen which will display weather data. I created the images and fonts folders into which I uploaded the files. I included them in pubspec.yaml. Created a constants file.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../services/location.dart';
-
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
